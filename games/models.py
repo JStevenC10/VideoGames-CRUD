f53c11c64ab8f7b4ff7d69f5a11a81a7378fa9ff
ChiquiPR <ChiquiPR@gmail.com>
2022-11-17 12:43:06 -0500

    models

--- games/models.py
+++ games/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=100, verbose_name='name')
+    plataforms = models.CharField(max_length=100, verbose_name='plataforms')
+    category = models.CharField(max_length=100, verbose_name='category')
+    image = models.ImageField(upload_to='image/', null=True, verbose_name='image')
+
+    def __str__(self):
+        presentation = f'{self.name} - {self.plataforms} - {self.category}'
+        return presentation  
+
+    def delete(self, using=None, keep_parents=False):
+        self.image.storage.delete(self.image.name)
+        super().delete()
